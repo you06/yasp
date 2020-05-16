0efb51e40a2eccfb8ed73387d8147dc06ec30996
you06 <you1474600@gmail.com>
2020-05-16 15:00:05 +0000

    add basic select
    Signed-off-by: you06 <you1474600@gmail.com>

---
+++ src/ast/dml.rs
@@ -0,0 +1,41 @@
+use std::fmt;
+
+#[allow(dead_code)]
+pub enum DMLNode {
+    Select(SelectNode),
+}
+
+pub enum Field {
+    Name(String),
+    All,
+}
+
+impl fmt::Debug for Field {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Field::Name(name) => write!(f, "{}", name),
+            Field::All => write!(f, "*"),
+        }
+    }
+}
+
+pub struct SelectNode {
+    pub fields: Vec<Field>,
+    pub result_table: String,
+}
+
+impl fmt::Debug for SelectNode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "select ")?;
+        let l = self.fields.len();
+        for index in 0..l {
+            write!(f, "{:?}", self.fields[index])?;
+            if index == l - 1 {
+                break;
+            }
+            write!(f, "{}", ",")?;
+        }
+        write!(f, " from {}", self.result_table)?;
+        Ok(())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,27 @@
+mod ast;
+
+#[macro_use]
+extern crate lalrpop_util;
+
+lalrpop_mod!(pub grammar);
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_select() {
+        let exprs = grammar::ExprsParser::new()
+            .parse("select * from sakura")
+            .unwrap();
+        assert_eq!(&format!("{:?}", exprs), "[select * from sakura]");
+
+        let exprs = grammar::ExprsParser::new()
+            .parse("select * from sakura; select rin, shizuku, * from uta")
+            .unwrap();
+        assert_eq!(
+            &format!("{:?}", exprs),
+            "[select * from sakura, select rin,shizuku,* from uta]"
+        );
     }
 }
